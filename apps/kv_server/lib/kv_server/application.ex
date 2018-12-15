65c632dd79c35b15eea134f7358e842c37d43be6
João Fernandes <joaovitorfernandes2@gmail.com>
2018-12-15 01:36:17 -0200

    Add gen_tcp echo server
    https://elixir-lang.org/getting-started/mix-otp/task-and-gen-tcp.html

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,10 +6,12 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg},
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
